3bf776bbfef25694d9818e870962eb019be02a90
Shah Md. Jihan <shahmohammadjihan@gmail.com>
2022-11-25 17:04:43 +0600

    create use api added

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mfyq6m8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+async function run() {
+  try {
+    // all collections
+    const usersCollection = client.db("eBikroy").collection("users");
+
+    // create user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 
 app.get("/", async (req, res) => {
   res.send("e-bikroy server is running");
