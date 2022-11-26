7eeadfa6e264dce689a0befff8a5e319ec6883a3
Shah Md. Jihan <shahmohammadjihan@gmail.com>
2022-11-26 16:39:35 +0600

    advertisement api and brand api added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
 const port = process.env.PORT || 5000;
 const app = express();
 require("dotenv").config();
@@ -16,11 +17,57 @@ async function run() {
   try {
     // all collections
     const usersCollection = client.db("eBikroy").collection("users");
+    const brandsCollection = client.db("eBikroy").collection("brands");
+    const addsCollection = client.db("eBikroy").collection("adds");
 
     // create user api
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // check admin api
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    // check seller api
+    app.get("/users/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
+    });
+
+    // add brand api
+    app.post("/brands", async (req, res) => {
+      const brands = req.body;
+      const result = await brandsCollection.insertOne(brands);
+      res.send(result);
+    });
+
+    // get brands api
+    app.get("/brands", async (req, res) => {
+      const query = {};
+      const brands = await brandsCollection.find(query).toArray();
+      res.send(brands);
+    });
+    // get single brands api
+    app.get("/brands/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const brand = await brandsCollection.findOne(query);
+      res.send(brand);
+    });
+
+    // add advertisement api
+    app.post("/post/add", async (req, res) => {
+      const addData = req.body;
+      const result = await addsCollection.insertOne(addData);
       res.send(result);
     });
   } finally {
